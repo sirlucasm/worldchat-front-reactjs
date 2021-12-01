2c106bbe060bf785f4221aefeace6780f65ee6bb
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-12-01 11:09:24 -0300

    fix: fixed _document crossOrigin warning
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
           <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet" />
-          <script src="https://kit.fontawesome.com/3f722b8912.js" crossOrigin="anonymous"></script>
+          <script src="https://kit.fontawesome.com/3f722b8912.js" crossOrigin={"anonymous"}></script>
         </Head>
         <body>
           <Main />
